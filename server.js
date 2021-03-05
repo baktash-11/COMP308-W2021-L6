2a31521cf56150706ab5df87a8abef6282de4e28
Baktash Sana <51730261+baktash-11@users.noreply.github.com>
2021-03-05 13:48:00 -0500

    reorganized folders and paths

--- server.js
+++ server.js
@@ -4,7 +4,7 @@
  * Module dependencies.
  */
 //server js requires app.js 
-let app = require('./app');
+let app = require('./server/config/app');
 let debug = require('debug')('comp308-w2021-l3b:server');
 let http = require('http');
 
--- server/config/app.js
+++ server/config/app.js
@@ -7,7 +7,7 @@ let logger = require('morgan');
 
 //Database setup --------------------------------------------------------
 let mongoose = require('mongoose');
-let DB = require('./config/db');
+let DB = require('./db');
 //point Mongoose to the db URI
 mongoose.connect(DB.URI);
 let mongoDB = mongoose.connection; 
@@ -19,22 +19,21 @@ mongoDB.once('open', () => {
 //-------------------------------------------------------------------------
 
 //requires(imports) routes index 
-let indexRouter = require('./routes/index');
-
+let indexRouter = require('../routes/index');
 //define our contact route
-let contactRouter = require('./routes/contact');
+let contactRouter = require('../routes/contact');
 
 let app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'node_modules')));
+app.use(express.static(path.join(__dirname, '../../public')));
+app.use(express.static(path.join(__dirname, '../../node_modules')));
 
 app.use('/', indexRouter);
 app.use('/contact-list', contactRouter);
